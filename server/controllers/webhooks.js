1cd75acd1c73beb6622d30ab2057f474626c9545
Shrutika Gorle <shrutikagorle0808@gmail.com>
2025-07-04 14:18:26 +0530

    lets try

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -12,8 +12,8 @@ export const clerkWebhooks = async (req, res) => {
     await whook.verify(JSON.stringify(req.body), {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
-      "svix-signature": req.headers["svix-signature"],
-    });
+      "svix-signature": req.headers["svix-signature"]
+    })
      console.log("🔔 Webhook received:", req.body);
     const { data, type } = req.body;
     switch (type) {
@@ -33,7 +33,7 @@ export const clerkWebhooks = async (req, res) => {
         const userData = {
           email: data.email_address[0].email_address,
           name: data.first_name + " " + data.last_name,
-          imageUrl: data.image_url,
+          imageUrl: data.image_url
         };
         await User.findByIdAndUpdate(data.id, userData);
         res.json({});
@@ -50,6 +50,6 @@ export const clerkWebhooks = async (req, res) => {
         break;
     }
   } catch (error) {
-    res.json({sussess: false, message:error.message})
+    res.json({success: false, message:error.message})
   }
 };
